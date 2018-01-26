83579d3b7cfeebae100e254ca5649acbf516d42a
VodorUrfs <Vodor@vodor.net>
2018-01-26 11:27:12 +0000

    added exit val

--- Hyper-V-Snapshot-Detection.ps1
+++ Hyper-V-Snapshot-Detection.ps1
@@ -12,9 +12,17 @@ if(test-path $snapshotpath)
 {
 
 Write-Host "<-Start Result->"
-write-host "CSMon_Result=Snapshot Detected"
+write-host "CSMon_Result=Snapshot Detected - Advise Engineer to Confirm"
 Write-Host "<-End Result->"
-
+$exit_val=1
 }
+else
+{
+write-host "<-Start Result->"
+Write-host "CSMON-ResultMessage=No Snapshot Found"
+write-host "<-End Result->"
+$exit_val=0
+}
+Exit $exit_val
 
 }
